17a44da8012e255b9472b0b326a9dad27c1a94c1
towa0 <126798279+towa0@users.noreply.github.com>
2023-11-28 16:03:33 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {unoptimized: true}
-}
+  distDir: "out",
+  output: "out",
+  images: { unoptimized: true },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
